2ebec2a04f97d941107b6232f8ee35357a4963d9
emmett c <emmett@christensons.net>
2025-07-23 10:39:03 -0500

    improved logic to be shorter and more readable

--- Slots_Machine_Proto_Code.ino
+++ Slots_Machine_Proto_Code.ino
@@ -6,13 +6,11 @@ const int button_pin = 16; // Button input pin
 
 const int SPR = 512; // Steps per revolution
 
-int ticks = 0;
 int current_step = 0;
-int state = 0;
 
-int end_pos_1;
-int end_pos_2;
-int end_pos_3;
+int remaining_1;
+int remaining_2;
+int remaining_3;
 
 int rand_pos() {
   // Returns a radom value from 512 to 1024 in increments of ~85
@@ -20,7 +18,6 @@ int rand_pos() {
 }
 
 void step_motor(const int pins[4]) {
-  int position = current_step % 4;
   digitalWrite(pins[0], position == 0 || position == 1);
   digitalWrite(pins[1], position == 1 || position == 2);
   digitalWrite(pins[2], position == 2 || position == 3);
@@ -46,54 +43,43 @@ void setup() {
 
   pinMode(button_pin, INPUT_PULLUP);
 
-  end_pos_1 = rand_pos(); end_pos_2 = rand_pos(); end_pos_3 = rand_pos();
-
-  ticks = 0;
+  // sets initial spin durations
+  remaining_1 = rand_pos();
+  remaining_2 = remaining_1 + rand_pos();
+  remaining_3 = remaining_2 + rand_pos();
 }
 
 void loop() {
-  switch (state) {
-    case 0:
-      step_motor(M1);
-      step_motor(M2);
-      step_motor(M3);
-      if (++ticks >= end_pos_1) {
-        ticks = 0;
-        state = 1;
-      }
-      break;
-    case 1:
-      stop_motor(M1);
-      step_motor(M2);
-      step_motor(M3);
-      if (++ticks >= end_pos_2) {
-        ticks = 0;
-        state = 2;
-      }
-      break;
-    case 2:
-      stop_motor(M1);
-      stop_motor(M2);
-      step_motor(M3);
-      if (++ticks >= end_pos_3) {
-        ticks = 0;
-        state = 3;
-      }
-      break;
-    case 3:
-      stop_motor(M1);
-      stop_motor(M2);
-      stop_motor(M3);
-      if (digitalRead(button_pin) == LOW) {
-        ticks = 0;
-        state = 0;
-        end_pos_1 = rand_pos();
-        end_pos_2 = rand_pos();
-        end_pos_3 = rand_pos();
-      }
-      break;
+  if (remaining_1 > 0) { // spins motor 1 if it has a remaining duration left
+    step_motor(M1);
+    remaining_1--;
+  } else {
+    stop_motor(M1);
   }
 
-  current_step++;
+  if (remaining_2 > 0) { // spins motor 1 if it has a remaining duration left
+    step_motor(M2);
+    remaining_2--;
+  } else {
+    stop_motor(M2);
+  }
+
+  if (remaining_3 > 0) { // spins motor 1 if it has a remaining duration left
+    step_motor(M3);
+    remaining_3--;
+    return;
+  } else {
+    stop_motor(M3);
+  }
+
+  // waits for button to reset
+  if (digitalRead(button_pin) == LOW) {
+    // set new spin durations
+    remaining_1 = rand_pos();
+    remaining_2 = remaining_1 + rand_pos();
+    remaining_3 = remaining_2 + rand_pos();
+  }
+
+  current_step = ++current_step % 4;
   delay(2);
 }

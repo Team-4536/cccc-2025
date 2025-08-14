af12786043b6f66b6798369eed1fd254117e63f1
davidorser <davidorser@gmail.com>
2025-08-14 09:18:02 -0500

    created static int state version of step_motor()

--- slot_machine/slot_machine.h
+++ slot_machine/slot_machine.h
@@ -1,4 +1,5 @@
 const int SPR = 513; // steps per revolution
+const int debug = false;
 
 const int wheel_items[3][5] = {
   {1, 2, 3, 4, 5},
@@ -13,10 +14,29 @@ int rand_pos(int motor, int &segment) {
   // return SPR;
 }
 
-void step_motor(const int pins[4], const int state) {  // TODO: Embed State internally using a static int
-  for (int i = 0; i < 4; i++) digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+void step_motor(const int pins[4], const int state) { // TODO: Embed State internally using a static int
+  for (int i = 0; i < 4; i++)
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+}
+
+void step_motor(const int pins[4]) {
+  static int state = 0;
+
+  state = state + 1;
+  if (state >= 4) { // counts 0, 1, 2, 3
+    state = 0;
+  }
+
+  if (debug) {Serial.print("Stepper State: ");}
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+    if (debug) {Serial.print(state == i || state == (i + 1) % 4);  Serial.print(",");}
+  }
+  if (debug) {Serial.println(" ");}
 }
 
 void stop_motor(const int pins[4]) {
-  for (int i = 0; i < 4; i++) digitalWrite(pins[i], LOW);
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], LOW);
+  }
 }

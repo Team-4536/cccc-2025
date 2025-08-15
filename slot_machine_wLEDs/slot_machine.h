6a484a604b1b332ac6c6b256553f2cd58ccd8602
davidorser <davidorser@gmail.com>
2025-08-15 11:32:39 -0500

    added simple one LED extension

---
+++ slot_machine_wLEDs/slot_machine.h
@@ -0,0 +1,88 @@
+const int SPR = 255; // steps per revolution
+const int debug = false;
+
+const int wheel_items[3][5] = {
+  {1, 2, 3, 4, 5},
+  {1, 2, 3, 4, 5},
+  {1, 2, 3, 4, 5}
+};
+
+
+int rand_pos(int motor, int &segment) {  // TODO: This doesn't take into account the current position of the wheel
+  int position = random(0, 5); // get a number from 0 to 4
+  segment = wheel_items[motor][position];  // Save the winning segment for display purposes
+  return SPR * (2 + (position / 5.0) + 2 * motor);  // return target motor position in number of steps
+}
+
+void begin_motors(const int pinsM1[4],const int pinsM2[4],const int pinsM3[4]) {
+  for (int i = 0; i < 4; i++) {
+    pinMode(pinsM1[i], OUTPUT);
+    pinMode(pinsM2[i], OUTPUT);
+    pinMode(pinsM3[i], OUTPUT);
+  }
+}
+
+
+void step_motorM1(const int pins[4]) {
+  static int state = 0;
+
+  state = state + 1;
+  if (state >= 4) { // counts 0, 1, 2, 3
+    state = 0;
+  }
+
+  if (debug) {Serial.print("Stepper State: ");}
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+    if (debug) {Serial.print(state == i || state == (i + 1) % 4);  Serial.print(",");}
+  }
+  if (debug) {Serial.println(" ");}
+}
+
+
+void step_motorM2(const int pins[4]) {
+  static int state = 0;
+
+  state = state + 1;
+  if (state >= 4) { // counts 0, 1, 2, 3
+    state = 0;
+  }
+
+  if (debug) {Serial.print("Stepper State: ");}
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+    if (debug) {Serial.print(state == i || state == (i + 1) % 4);  Serial.print(",");}
+  }
+  if (debug) {Serial.println(" ");}
+}
+
+
+void step_motorM3(const int pins[4]) {
+  static int state = 0;
+
+  state = state + 1;
+  if (state >= 4) { // counts 0, 1, 2, 3
+    state = 0;
+  }
+
+  if (debug) {Serial.print("Stepper State: ");}
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+    if (debug) {Serial.print(state == i || state == (i + 1) % 4);  Serial.print(",");}
+  }
+  if (debug) {Serial.println(" ");}
+}
+
+
+void stop_motor(const int pins[4]) {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], LOW);
+  }
+}
+
+
+void step_motor(const int pins[4], const int state) { // TODO: Embed State internally using a static int
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(pins[i], state == i || state == (i + 1) % 4);
+  }
+}
---
+++ slot_machine_wLEDs/slot_machine_wLEDs.ino
@@ -0,0 +1,79 @@
+#include "slot_machine.h"
+
+const int M1[4] = {2, 3, 4, 5}; // motor 1 pins
+const int M2[4] = {6, 7, 8, 9}; // motor 2 pins
+const int M3[4] = {10, 11, 12, 13}; // motor 3 pins
+
+const int button_pin = 16; // button input pin
+const int led_pin = 15; // button input pin
+
+int remaining_1 = 0;
+int remaining_2 = 0;
+int remaining_3 = 0;
+
+int seg_1 = 1;
+int seg_2 = 2;
+int seg_3 = 3;
+
+void setup() {
+  randomSeed(analogRead(0));
+  
+  pinMode(led_pin, OUTPUT);
+  pinMode(button_pin, INPUT_PULLDOWN);
+
+  begin_motors(M1,M2,M3);
+  Serial.begin();
+}
+
+void loop() {
+  if (remaining_1 > 0) { // spins motor 1 if it has a remaining duration left
+    step_motorM1(M1);
+    remaining_1--;
+  } else {
+    stop_motor(M1);
+  }
+
+  if (remaining_2 > 0) { // spins motor 2 if it has a remaining duration left
+    step_motorM2(M2);
+    remaining_2--;
+  } else {
+    stop_motor(M2);
+  }
+
+  if (remaining_3 > 0) { // spins motor 3 if it has a remaining duration left
+    step_motorM3(M3);
+    remaining_3--;
+  } else {
+    stop_motor(M3);
+  }
+
+  if (remaining_1 <= 0 && remaining_2 <= 0 && remaining_3 <= 0) {
+    Serial.print("spin:");  // TODO: Figure out why we're only spinning across 3 values instead of 5 values
+    Serial.print(seg_1);
+    Serial.print(seg_2);
+    Serial.print(seg_3);
+
+    if (seg_1 == seg_2 && seg_2 == seg_3) {
+      Serial.println(", triple win!!!");
+      digitalWrite(led_pin,HIGH);
+    } else if (seg_1 == seg_2 || seg_2 == seg_3 || seg_3 == seg_1) {
+      Serial.println(", double win!!");
+      digitalWrite(led_pin,HIGH);
+    } else {
+      Serial.println(". No luck, try again...");
+      digitalWrite(led_pin,LOW);
+    }
+
+     // Do nothing and wait until the button is pressed.
+    if (digitalRead(button_pin) == LOW) {  
+      delay(100); 
+    } else { 
+      // roll three new spins!
+      remaining_1 = rand_pos(0, seg_1);
+      remaining_2 = rand_pos(1, seg_2);
+      remaining_3 = rand_pos(2, seg_3);
+    }
+  }
+
+  delay(4);
+}

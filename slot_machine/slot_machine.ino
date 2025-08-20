f5e934996e6e837bc6f15a59b24b81f16e8391a5
Lil Sack <remove>
2025-08-19 22:49:38 -0500

    minor changes to follow manual

--- slot_machine/slot_machine.ino
+++ slot_machine/slot_machine.ino
@@ -1,8 +1,8 @@
 #include "SlotHeader.h"
 
-int M1[4] = {2, 3, 4, 5}; // motor 1 pins
-int M2[4] = {6, 7, 8, 9}; // motor 2 pins
-int M3[4] = {10, 11, 12, 13}; // motor 3 pins
+int motor_1_pins[4] = {2, 3, 4, 5}; // motor 1 pins
+int motor_2_pins[4] = {6, 7, 8, 9}; // motor 2 pins
+int motor_3_pins[4] = {10, 11, 12, 13}; // motor 3 pins
 
 int button_pin = 16; // button input pin
 
@@ -29,7 +29,7 @@ bool resultPrinted = true; // tells the pi whether or not the result was printed
 
 void setup() {
   Serial.begin();                      // serial monitor
-  begin_motors(M1,M2,M3);              // motors
+  begin_motors(motor_1_pins,motor_2_pins,motor_3_pins);              // motors
   pinMode(button_pin, INPUT_PULLDOWN); // button
 }
 
@@ -37,24 +37,24 @@ void setup() {
 
 void loop() {
   if (remaining_1 > 0) { // spins motor 1 if it has a remaining duration left
-    step_motorM1(M1);
+    step_motorM1(motor_1_pins);
     remaining_1 = remaining_1 - 1;
   }else {
-    stop_motor(M1);
+    stop_motor(motor_1_pins);
   }
 
   if (remaining_2 > 0) { // spins motor 2 if it has a remaining duration left
-    step_motorM2(M2);
+    step_motorM2(motor_2_pins);
     remaining_2 = remaining_2 - 1;
   }else {
-    stop_motor(M2);
+    stop_motor(motor_2_pins);
   }
 
   if (remaining_3 > 0) { // spins motor 3 if it has a remaining duration left
-    step_motorM3(M3);
+    step_motorM3(motor_3_pins);
     remaining_3 = remaining_3 - 1;
   }else {
-    stop_motor(M3);
+    stop_motor(motor_3_pins);
   }
   
 
@@ -63,7 +63,7 @@ void loop() {
 
     // checks if the computer has already printed the result, if it hasn't then it prints it
     if (resultPrinted == false){
-      Serial.print("spin: ");  
+      Serial.print("Spin: ");  
       Serial.print(seg_1);
       Serial.print(seg_2);
       Serial.print(seg_3);
@@ -71,8 +71,8 @@ void loop() {
       if (seg_1 == 1 && seg_2 == 1 && seg_3 == 1) {                     // All three segments are 1 (starting position)
         Serial.println(", JACKPOT!!!!!");
       }else if (seg_1 == seg_2 && seg_2 == seg_3){                     // All three segments are the same
-        Serial.println(", triple win!!!");
-      }else if (seg_1 == seg_2 || seg_2 == seg_3 || seg_3 == seg_1) {  // Any two segments are the same
+        Serial.println(", TRIPLE win!!!");
+      }else if (seg_1 == seg_2 || seg_2 == seg_3 || seg_1 == seg_3) {  // Any two segments are the same
         Serial.println(", double win!!");
       }else {                                                          // None are the same
         Serial.println(". No luck, try again...");
